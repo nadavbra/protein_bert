244e9d02ac8813b106656f1f1ee3362435f2a131
Nadav Brandes <nadavb@cs.huji.ac.il>
2021-05-14 01:20:26 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = 'protein-bert',
+    version = '1.0.0',
+    description = 'A BERT-like deep language model for protein sequences.',
+    url = 'https://github.com/nadavbra/protein_bert',
+    author = 'Nadav Brandes',
+    author_email  ='nadav.brandes@mail.huji.ac.il',
+    packages = ['proteinbert', 'proteinbert.shared_utils'],
+    license = 'MIT',
+    scripts = [
+        'bin/create_uniref_db',
+        'bin/create_uniref_h5_dataset',
+        'bin/pretrain_proteinbert',
+        'bin/set_h5_testset',
+    ],
+    install_requires = [
+        'tensorflow',
+        'numpy',
+        'pandas',
+        'h5py',
+        'lxml',
+        'pyfaidx',
+    ],
+)
